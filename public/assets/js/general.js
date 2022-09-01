3a9509e4f63b0a2c6740caf7d82b8d523abd595b
honore8 <57352746+honore8@users.noreply.github.com>
2022-09-01 18:51:24 +0000

    --

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -1,5 +1,5 @@
 //enter the count down date using the format year, month, day, time:time
-$(".count-down").ccountdown(2022,9,24,'16:29');
+$(".count-down").ccountdown(2022,9,30,'16:29');
 
 // Utilizing the Modernizr object created to implement placeholder functionality
 function hasPlaceholderSupport() {
